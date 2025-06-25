c38fd2b700ed43965fd94a112d520ee64868b62a
kurudoqqe <vadim.firedog@gmail.com>
2025-06-25 16:52:59 +0300

    add help card section

--- src/views/home-page/HomeView.vue
+++ src/views/home-page/HomeView.vue
@@ -2,17 +2,25 @@
 import ServicesContainer from "@/views/home-page/components/ServicesContainer.vue";
 import VideoContainer from "@/views/home-page/components/VideoContainer.vue";
 import GradientBackground1 from "@/assets/images/background1.png";
+import GradientBackground2 from "@/assets/images/background2.png";
+import HelpCard from "@/views/home-page/components/HelpCard.vue";
 import Header from "@/components/header/HeaderComponent.vue";
 </script>
 
 <template>
   <article
-    class="first-container"
     :style="{ backgroundImage: `url(${GradientBackground1})` }"
+    class="first-container"
   >
     <Header />
     <VideoContainer />
     <ServicesContainer />
+  </article>
+  <article
+    :style="{ backgroundImage: `url(${GradientBackground2})` }"
+    class="second-container"
+  >
+    <HelpCard />
   </article>
 </template>
 
@@ -20,9 +28,13 @@ import Header from "@/components/header/HeaderComponent.vue";
 @use "@/assets/global/variables";
 @use "sass:map";
 
-.first-container {
+article {
   background-size: 100% 100%;
   background-position: center;
   background-repeat: no-repeat;
+}
+
+.second-container {
+  padding-top: 200px;
 }
 </style>
---
+++ src/views/home-page/components/HelpCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+import GradientBackground from "@/assets/images/card-background.png";
+import Button from "@/components/button/ButtonComponent.vue";
+</script>
+
+<template>
+  <section
+    class="help-card page-container"
+    :style="{ background: `url(${GradientBackground})` }"
+  >
+    <h1>не знаете что выбрать?</h1>
+    <Button class="help-button">
+      <p class="text-1">Заполнить заявку</p>
+    </Button>
+    <p class="text-1">и мы вам подскажем</p>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.help-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+
+  width: 100%;
+  height: 570px;
+
+  margin: 0 auto;
+}
+
+.help-button {
+  margin-top: 20px;
+  padding: 0.65rem 6.25rem;
+}
+</style>
