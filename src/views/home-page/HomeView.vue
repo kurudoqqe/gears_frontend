7df3f088e32193c372fd3f8549c22170f16217b2
kurudoqqe <vadim.firedog@gmail.com>
2025-06-27 00:11:05 +0300

    add blog section

--- src/views/home-page/HomeView.vue
+++ src/views/home-page/HomeView.vue
@@ -6,10 +6,12 @@ import FeedbackSection from "@/views/home-page/components/FeedbackSection.vue";
 import VideoContainer from "@/views/home-page/components/VideoContainer.vue";
 import WorkProcess from "@/views/home-page/components/WorkProcess.vue";
 import TeamSection from "@/views/home-page/components/TeamSection.vue";
+import BlogSection from "@/views/home-page/components/BlogSection.vue";
 import GradientBackground1 from "@/assets/images/background1.png";
 import GradientBackground2 from "@/assets/images/background2.png";
 import GradientBackground3 from "@/assets/images/background3.png";
 import GradientBackground4 from "@/assets/images/background4.png";
+import GradientBackground5 from "@/assets/images/background5.png";
 import HelpCard from "@/views/home-page/components/HelpCard.vue";
 import Header from "@/components/header/HeaderComponent.vue";
 </script>
@@ -44,6 +46,12 @@ import Header from "@/components/header/HeaderComponent.vue";
   >
     <FeedbackSection />
   </article>
+  <article
+    :style="{ backgroundImage: `url(${GradientBackground5})` }"
+    class="fifth-container"
+  >
+    <BlogSection />
+  </article>
 </template>
 
 <style scoped lang="scss">
@@ -64,5 +72,10 @@ article {
 .fourth-container {
   padding-top: 145px;
   padding-bottom: 200px;
+}
+
+.fifth-container {
+  padding-top: 60px;
+  padding-bottom: 240px;
 }
 </style>
---
+++ src/views/home-page/components/BlogSection.vue
@@ -0,0 +1,57 @@
+<script setup></script>
+
+<template>
+  <section class="blog page-container">
+    <h1>Блог</h1>
+    <div class="blog-content">
+      <div class="blog-video">
+        <video controls />
+        <p class="text-2">Подкаст “Как стать Python синьором в 19”</p>
+      </div>
+      <div class="blog-video">
+        <video controls />
+        <p class="text-2">Узнаем у людей на улице, кто из них в ИТ</p>
+      </div>
+      <div class="blog-video">
+        <video controls />
+        <p class="text-2">Подкаст “Как стать Python синьором в 19”</p>
+      </div>
+      <div class="blog-video">
+        <video controls />
+        <p class="text-2">Узнаем у людей на улице, кто из них в ИТ</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/global/variables";
+@use "sass:map";
+
+.blog {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+
+  margin: 0 auto;
+}
+
+.blog-content {
+  display: flex;
+  gap: 20px;
+}
+
+.blog-video {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+
+  width: 280px;
+
+  > video {
+    border: 1px solid map.get(variables.$color, white);
+
+    height: 280px;
+  }
+}
+</style>
