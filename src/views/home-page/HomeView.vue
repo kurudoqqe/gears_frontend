a64044cf738b4e6709948bedd05be3b94caa9af3
kurudoqqe <vadim.firedog@gmail.com>
2025-10-17 04:36:24 +0300

    hide video container & add typewriter title

--- src/views/home-page/HomeView.vue
+++ src/views/home-page/HomeView.vue
@@ -5,10 +5,10 @@ import PortfolioComponent from "@/views/home-page/components/PortfolioComponent.
 import ServicesContainer from "@/views/home-page/components/ServicesContainer.vue";
 import ApplicationButton from "@/views/home-page/components/ApplicationButton.vue";
 import FeedbackSection from "@/views/home-page/components/FeedbackSection.vue";
-import VideoContainer from "@/views/home-page/components/VideoContainer.vue";
 import WorkProcess from "@/views/home-page/components/WorkProcess.vue";
 import TeamSection from "@/views/home-page/components/TeamSection.vue";
 import BlogSection from "@/views/home-page/components/BlogSection.vue";
+import PageTitle from "@/views/home-page/components/PageTitle.vue";
 import GradientBackground1 from "@/assets/images/background1.png";
 import GradientBackground2 from "@/assets/images/background2.png";
 import GradientBackground3 from "@/assets/images/background3.png";
@@ -34,7 +34,8 @@ const toggleMenu = () => {
       class="first-container"
     >
       <Header :toggle-menu="toggleMenu" />
-      <VideoContainer />
+      <!--      <VideoContainer />-->
+      <PageTitle />
       <ServicesContainer />
     </article>
     <article
